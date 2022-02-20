94487769fc2f2bf9b628843c1bc13298b33a4fd6
Kenny <kennymonique972@gmail.com>
2022-02-20 12:32:02 +0100

    add properties in User entity + add inputs as well in RegisterType

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +15,40 @@ class RegisterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password');
+            ->add('firstname', TextType::class, [
+                'label' => 'Votre prénom',
+                'attr'  => [
+                    'placeholder' => "Veuillez saisir votre prénom"
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Votre nom',
+                'attr'  => [
+                    'placeholder' => "Veuillez saisir votre nom"
+                ]
+            ])
+            ->add('email', TextType::class, [
+                'label' => 'Votre adresse électronique',
+                'attr'  => [
+                    'placeholder' => "Veuillez saisir votre adresse électronique"
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => "Votre mot de passe",
+                'attr'  => [
+                    'placeholder' => 'Veuillez saisir votre mot de passe'
+                ]
+            ])
+            ->add('password_confirm', PasswordType::class, [
+                'label' => "Confirmation de votre mot de passe",
+                'mapped' => false,
+                'attr'  => [
+                    'placeholder' => 'Veuillez saisir de nouveau votre mot de passe'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => "S'inscrire"
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
